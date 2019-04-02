7f35625bf34d83340d890871a26ef7ab36ca85fc
Radina Matic <radina.matic@gmail.com>
2019-04-02 13:47:31 -0700

    Spaces for tabs

--- docs/conf.py
+++ docs/conf.py
@@ -501,10 +501,10 @@ pdf_splittables = False
 #pdf_page_template = 'cutePage'
 
 # Show Table Of Contents at the beginning?
-pdf_use_toc = True
+#pdf_use_toc = True
 
 # How many levels deep should the table of contents be?
-pdf_toc_depth = 3
+#pdf_toc_depth = 3
 
 # Add section number to section references
 pdf_use_numbered_links = False
